f9408f378725179d7492423b140b39f59d289f77
Gede Lumbung <gedesumawijaya@gmail.com>
2016-09-19 05:03:39 +0800

    fix space tabs

--- scripts/options.js
+++ scripts/options.js
@@ -1,14 +1,13 @@
-
 $(document).ready(function(){
 	var storage = chrome.storage.sync;
 
-    storage.get(["mysmartfren"], function(items){
-        if(items.mysmartfren){
-            var data = items.mysmartfren;
-            $("#imsi").val(data.imsi);
-            $("#token").val(data.token);
-        }
-    });
+	storage.get(["mysmartfren"], function(items){
+		if(items.mysmartfren){
+			var data = items.mysmartfren;
+			$("#imsi").val(data.imsi);
+			$("#token").val(data.token);
+		}
+	});
 
 	$("#button").click(function(){
 		var imsi = $("#imsi").val();
@@ -21,10 +20,9 @@ $(document).ready(function(){
 				'imsi' : $("#imsi").val(),
 				'token' : $("#token").val(),
 			}
-		    storage.set({'mysmartfren': params}, function() {
-		      $("#message").html("Success save your configuration.");
-		    });
+			storage.set({'mysmartfren': params}, function() {
+				("#message").html("Success save your configuration.");
+			});
 		}
-
 	});
 });
