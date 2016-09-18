05b50334b8a6b037257dfe889e946c507724e3fe
Gede Lumbung <gedesumawijaya@gmail.com>
2016-09-19 01:27:46 +0800

    split package info, main internet quota and bonus info

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,15 +1,25 @@
 $(document).ready(function(){
+    $("#loading").show();
+    $("#content").hide();
     $("#smartfren").submit( function () {    
-      $.post(
-       'https://my.smartfren.com/api/device/profile.php',
-        $(this).serialize(),
-        function(data){
-          var $result = $(data).find('div > center > table');
-          console.log($result)
-          $("#results").html($result[2].innerHTML)
-        }
-      );
-      return false;   
+        $.post(
+            'https://my.smartfren.com/api/device/profile.php',
+            $(this).serialize(),
+            function(data){
+                var $result = $(data).find('div > center > table');
+
+                $("#packageInfo").html($result[0].innerHTML);
+
+                $("#mainQuota").html($result[1].innerHTML);
+                //remove button, to prevent user click
+                $("#mainQuota tr").eq(2).remove();
+
+                $("#bonus").html($result[2].innerHTML);
+                $("#loading").hide();
+                $("#content").show();
+            }
+        );
+        return false;   
     });
     $("#smartfren").submit();
 });
