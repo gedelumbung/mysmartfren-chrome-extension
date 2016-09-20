89c9620496e1ecd381169508115b4920d818a340
Gede Lumbung <gedesumawijaya@gmail.com>
2016-09-21 03:52:21 +0800

    multiple config

--- scripts/options.js
+++ scripts/options.js
@@ -1,15 +1,50 @@
 $(document).ready(function(){
 	var storage = chrome.storage.sync;
+	var data;
+	var action = 'add';
+	var index;
+
+	$("#saveButton").hide();
+	$("#removeButton").hide();
+	$("#addButton").show();
 
 	storage.get(["mysmartfren"], function(items){
 		if(items.mysmartfren){
-			var data = items.mysmartfren;
-			$("#imsi").val(data.imsi);
-			$("#token").val(data.token);
+			data = items.mysmartfren;
+		    $.each(data, function (i, data) {
+			    $('#configurationList').append($('<option>', { 
+			        value: i,
+			        text : data.title 
+			    }));
+			});
 		}
 	});
 
-	$("#button").click(function(){
+	$("#configurationList").change(function(){
+		if(this.value === ""){
+			$("#title").val('');
+			$("#imsi").val('');
+			$("#token").val('');
+			action = 'add';
+			$("#saveButton").hide();
+			$("#removeButton").hide();
+			$("#addButton").show();
+			return false;
+		}
+		$("#saveButton").show();
+		$("#removeButton").show();
+		$("#addButton").hide();
+
+		action = 'edit';
+		index = this.value;
+		var value = data[this.value];
+		$("#title").val(value.title);
+		$("#imsi").val(value.imsi);
+		$("#token").val(value.token);
+	});
+
+	$("#saveButton").click(function(){
+		var title = $("#title").val();
 		var imsi = $("#imsi").val();
 		var token = $("#token").val();
 		if(imsi === "" || token === ""){
@@ -17,10 +52,11 @@ $(document).ready(function(){
 		}
 		else{
 			var params = {
+				'title' : $("#title").val(),
 				'imsi' : $("#imsi").val(),
 				'token' : $("#token").val(),
 			}
-			storage.set({'mysmartfren': params}, function() {
+			storage.set({'mysmartfren': [params]}, function() {
 				$("#message").html("Success save your configuration.");
 			});
 		}
