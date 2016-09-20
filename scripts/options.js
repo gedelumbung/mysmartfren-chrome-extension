2a13d3002bab4837db4095bd88512018941de1e8
Gede Lumbung <gedesumawijaya@gmail.com>
2016-09-21 04:08:12 +0800

    enable to save multiple configuration

--- scripts/options.js
+++ scripts/options.js
@@ -8,17 +8,27 @@ $(document).ready(function(){
 	$("#removeButton").hide();
 	$("#addButton").show();
 
-	storage.get(["mysmartfren"], function(items){
-		if(items.mysmartfren){
-			data = items.mysmartfren;
-		    $.each(data, function (i, data) {
-			    $('#configurationList').append($('<option>', { 
-			        value: i,
-			        text : data.title 
+	load();
+
+	function load(){
+		storage.get(["mysmartfren"], function(items){
+			if(items.mysmartfren){
+				data = items.mysmartfren;
+				$('#configurationList').html('');
+				$('#configurationList').append($('<option>', { 
+			        value: '',
+			        text : '- Select Configuration -'
 			    }));
-			});
-		}
-	});
+
+			    $.each(data, function (i, value) {
+				    $('#configurationList').append($('<option>', { 
+				        value: i,
+				        text : value.title 
+				    }));
+				});
+			}
+		});
+	}
 
 	$("#configurationList").change(function(){
 		if(this.value === ""){
@@ -51,13 +61,35 @@ $(document).ready(function(){
 			$("#message").html("Please fill all fields.");
 		}
 		else{
+			data[index] = {
+				'title' : $("#title").val(),
+				'imsi' : $("#imsi").val(),
+				'token' : $("#token").val(),
+			};
+			storage.set({'mysmartfren': data}, function() {
+				$("#message").html("Success save your configuration.");
+				load();
+			});
+		}
+	});
+
+	$("#addButton").click(function(){
+		var title = $("#title").val();
+		var imsi = $("#imsi").val();
+		var token = $("#token").val();
+		if(imsi === "" || token === ""){
+			$("#message").html("Please fill all fields.");
+		}
+		else{
 			var params = {
 				'title' : $("#title").val(),
 				'imsi' : $("#imsi").val(),
 				'token' : $("#token").val(),
-			}
-			storage.set({'mysmartfren': [params]}, function() {
+			};
+			data.push(params);
+			storage.set({'mysmartfren': data}, function() {
 				$("#message").html("Success save your configuration.");
+				load();
 			});
 		}
 	});
